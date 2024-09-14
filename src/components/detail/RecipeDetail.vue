0a87bb54f62a4ff710929d6513850cd89c5834c0
malikizaki94 <malikizakitop@gmail.com>
2024-09-14 19:48:05 +0700

    tc

--- src/components/detail/RecipeDetail.vue
+++ src/components/detail/RecipeDetail.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="col-12 col-sm-8">
     <recipe-description></recipe-description>
+    <recipe-ingredients ></recipe-ingredients >
+        <recipe-directions ></recipe-directions >
   </div>
 </template>
 <script setup>
 import RecipeDescription from "./RecipeDescription.vue";
+import RecipeDirections from "./RecipeDirections.vue"
+import RecipeIngredients from "./RecipeIngredient.vue"
 import { onMounted } from "vue";
 import { useRoute } from "vue-router";
 import { useStore } from "vuex";
