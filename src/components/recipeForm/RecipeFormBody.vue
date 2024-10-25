f846150b430c53cb95d07009f46b6a4aebdbd64f
malikizaki94 <malikizakitop@gmail.com>
2024-10-25 18:31:21 +0700

    fc

---
+++ src/components/recipeForm/RecipeFormBody.vue
@@ -0,0 +1,141 @@
+<template>
+  <li class="list-group-item">
+    <form>
+      <!-- General Information Start -->
+      <div>
+        <p class="my-3 fs-5 fw-semibold">General Information</p>
+        <div>
+          <!-- Image Start -->
+          <div class="mb-3">
+            <BaseInput type="file" identity="recipeImage" label="Photo Recipe" />
+          </div>
+          <!-- Image End -->
+
+          <!-- Recipe Title Start -->
+          <div class="mb-3">
+            <BaseInput type="text" identity="recipeTitle" placeholder="Give your recipe a title" label="Recipe Title" />
+          </div>
+          <!-- Recipe Title End -->
+
+          <!-- Recipe Description Start -->
+          <div class="mb-3">
+            <BaseTextArea identity="recipeDescription" label="Description" placeholder="Share story behind recipe and what makes it special" />
+          </div>
+          <!-- Recipe Description End -->
+
+          <!-- Recipe Category Start -->
+          <div class="mb-3">
+            <BaseSelect :data="['Breakfast', 'Lunch', 'Dinner', 'Meals', 'Snacks']" />
+          </div>
+          <!-- Recipe Category End -->
+        </div>
+      </div>
+      <!-- General Information End -->
+
+      <!-- Time Setting Start -->
+      <div class="border-top py-1">
+        <p class="my-3 fs-5 fw-semibold">Time Setting</p>
+        <div>
+          <!-- Preparation Time Start -->
+          <div class="mb-3">
+            <BaseInput type="number" identity="prepTime" placeholder="0" label="Prep Time" />
+          </div>
+          <!-- Preparation Time End -->
+
+          <!-- Cook Time Start -->
+          <div class="mb-3">
+            <BaseInput type="number" identity="cookTime" placeholder="0" label="Cook Time" />
+          </div>
+          <!-- Cook Time End -->
+
+          <!-- Total Time Start -->
+          <div class="mb-3">
+            <BaseInput type="number" identity="totalTime" placeholder="0" label="Total Time" />
+          </div>
+          <!-- Total Time End -->
+        </div>
+      </div>
+      <!-- Time Setting End -->
+
+      <!-- Ingredients Start -->
+      <div class="border-top py-1">
+        <p class="my-3 fs-5 fw-semibold">Ingredients</p>
+        <p>
+          Enter one ingredient per line. Include the quantity (i.e. cups,
+          tablespoons) and any special preparation (i.e. sifted, softened,
+          chopped). Use optional headers to organize the different parts of the
+          recipe (i.e. Cake, Frosting, Dressing).
+        </p>
+        <div>
+          <div class="mb-3 row">
+            <!-- Ingredient Input Start -->
+            <div class="col-lg-11 col-11">
+              <BaseInput type="text" identity="ingredient" placeholder="Ex: 1 cup sugar" />
+            </div>
+            <!-- Ingredient Input End -->
+            <div
+              class="col-lg-1 col-1 col-form-label align-self-end delete-ingredient"
+              style="color: #cb3a31"
+            >
+              <i class="fa-regular fa-trash-can px-1"></i
+              ><span class="d-none d-md-inline">Delete</span>
+            </div>
+          </div>
+        </div>
+        <!-- Add More Button Start -->
+        <BaseButton class="new-ingredient-btn px-3 py-2" type="button">Add More</BaseButton>
+        <!-- Add More Button End -->
+      </div>
+      <!-- Ingredients End -->
+
+      <!-- Direction Start -->
+      <div class="border-top my-3">
+        <p class="my-3 fs-5 fw-semibold">Directions</p>
+        <p>
+          Explain how to make your recipe, including oven temperatures, baking
+          or cooking times, and pan sizes, etc. Use optional headers to organize
+          the different parts of the recipe
+        </p>
+        <div>
+          <div class="mb-3 row">
+            <!-- Direction Input Start -->
+            <div class="col-lg-11 col-11">
+              <BaseInput type="text" identity="direction" placeholder="Ex: 1 cup sugar" />
+            </div>
+            <!-- Direction Input End -->
+            <div
+              class="col-lg-1 col-1 col-form-label align-self-end delete-ingredient"
+              style="color: #cb3a31"
+            >
+              <i class="fa-regular fa-trash-can px-1"></i
+              ><span class="d-none d-md-inline">Delete</span>
+            </div>
+          </div>
+        </div>
+        <!-- Add More Button Start -->
+        <BaseButton class="new-ingredient-btn px-3 py-2" type="button">Add More</BaseButton>
+        <!-- Add More Button End -->
+      </div>
+      <!-- Direction End -->
+
+      <!-- Form Button Start -->
+      <div class="border-top py-3 d-flex my-4 justify-content-end">
+        <!-- Cancel Button Start -->
+        <BaseButton class="cancel-btn px-3 py-2 ms-1">Cancel</BaseButton>
+        <!-- Cancel Button End -->
+
+        <!-- Submit Button Start -->
+        <BaseButton class="submit-recipe-btn px-3 py-2 ms-1">Submit</BaseButton>
+        <!-- Submit Button End -->
+      </div>
+      <!-- Form Button End -->
+    </form>
+  </li>
+</template>
+
+<script setup>
+import BaseInput from "../ui/BaseInput.vue";
+import BaseTextArea from "../ui/BaseTextArea.vue";
+import BaseSelect from "../ui/BaseSelect.vue";
+import BaseButton from "../ui/BaseButton.vue";
+</script>
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import LoginPage from "./components/pages/LoginPage.vue"
 import SignupPage from "./components/pages/SignupPage.vue"
 import DetailPage from "./components/pages/DetailPage.vue"
 import UserPage from "./components/pages/UserPage.vue"
+import NewRecipePage from "./components/pages/NewRecipePage.vue";
 import Cookies from "js-cookie"
 // import { store } from "./store/index";
 
@@ -12,22 +13,6 @@ const checkAuth = () => {
     const expirationDate = Cookies.get("tokenExpirationDate");
     const userId = Cookies.get("UID");
 };
-
-// if (jwtCookie) {
-//     if (new Date().getTime() < +expirationDate) {
-//         store.commit("auth/setToken", {
-//             idToken: jwtCookie,
-//             expiresIn: expirationDate,
-//         });
-//         store.dispatch("auth/getUser", userId);
-//         return true;
-//     } else {
-//         store.commit("auth/setUserLogout");
-//         return false;
-//     }
-// } else {
-//     return false;
-// }
 
 export const routes = [
     {
@@ -47,8 +32,30 @@ export const routes = [
         path: "/user/:component",
         name: "userPage",
         component: UserPage,
+        beforeEnter: (to, from, next) => {
+            checkAuth() ? next() : next({ name: "login" });
+        },
+    },
+    {
+        path: "/new-recipe", name: "newRecipePage", component: NewRecipePage,
         // beforeEnter: (to, from, next) => {
         //     checkAuth() ? next() : next({ name: "login" });
         // },
-    }
+    },
 ]
+
+// if (jwtCookie) {
+//     if (new Date().getTime() < +expirationDate) {
+//         store.commit("auth/setToken", {
+//             idToken: jwtCookie,
+//             expiresIn: expirationDate,
+//         });
+//         store.dispatch("auth/getUser", userId);
+//         return true;
+//     } else {
+//         store.commit("auth/setUserLogout");
+//         return false;
+//     }
+// } else {
+//     return false;
+// }
